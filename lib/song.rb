aba72411daa166aee990647b60a99ceace525a78
Jessica Natovich <jessicanatovich@yahoo.com>
2019-12-11 22:41:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
-class Artist
-  attr_accessor :name
+class Song
+  attr_accessor :name, :artist
 
   @@all = []
 
@@ -12,20 +12,7 @@ class Artist
     @@all
   end
 
-  def add_song(song)
-    song.artist = self
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
-
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-
-  def self.song_count
-    Song.all.count
+  def artist_name
+    artist.name if artist
   end
 end
