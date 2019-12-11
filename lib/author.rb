743d6aad358f663d99cc81d005f1cf1f1425d0be
Jessica Natovich <jessicanatovich@yahoo.com>
2019-12-11 22:40:23 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,6 @@
-class Artist
+require 'pry'
+
+class Author
   attr_accessor :name
 
   @@all = []
@@ -12,20 +14,20 @@ class Artist
     @@all
   end
 
-  def add_song(song)
-    song.artist = self
+  def add_post(post)
+    post.author = self
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 
-  def self.song_count
-    Song.all.count
+  def self.post_count
+    Post.all.count
   end
 end
